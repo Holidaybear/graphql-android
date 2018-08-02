32ef7bc63831168f5b0f64cef5326d013f819847
Kami <kami.chen@honestbee.com>
2018-08-02 18:20:55 +0800

    Modify call for last week date

--- app/src/main/java/tw/holidaybear/graphql/android/data/TrendRepository.kt
+++ app/src/main/java/tw/holidaybear/graphql/android/data/TrendRepository.kt
@@ -5,14 +5,18 @@ import com.apollographql.apollo.rx2.Rx2Apollo
 import io.reactivex.Observable
 import tw.holidaybear.graphql.android.TrendQuery
 import tw.holidaybear.graphql.android.type.SearchType
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TrendRepository(private val apolloClient: ApolloClient) {
 
     fun getTrends(): Observable<List<Repo>> {
+        val lastWeekDate = Calendar.getInstance().timeInMillis - 24 * 3600 * 1000 * 7
+        val formattedDateText = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(lastWeekDate)
 
         val trendQuery = TrendQuery.builder()
                 .first(25)
-                .query("created:>2018-07-15 sort:stars-desc")
+                .query("created:>$formattedDateText sort:stars-desc")
                 .type(SearchType.REPOSITORY)
                 .build()
 
