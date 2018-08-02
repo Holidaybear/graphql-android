cc2691e861bd2f5252fbcf7424fb146f69235695
Kami <kami.chen@honestbee.com>
2018-08-02 15:48:54 +0800

    Use data binding for main page

--- app/src/main/java/tw/holidaybear/graphql/android/repos/MainActivity.kt
+++ app/src/main/java/tw/holidaybear/graphql/android/repos/MainActivity.kt
@@ -1,39 +1,31 @@
 package tw.holidaybear.graphql.android.repos
 
 import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.Toolbar
 import android.view.View
-import android.widget.ProgressBar
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import tw.holidaybear.graphql.android.R
+import tw.holidaybear.graphql.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var toolbar: Toolbar
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var progress: ProgressBar
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var adapter: ReposAdapter
     private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        toolbar = findViewById(R.id.toolbar)
-        recyclerView = findViewById(R.id.recyclerview)
-        progress = findViewById(R.id.progress)
-
-        setSupportActionBar(toolbar)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         adapter = ReposAdapter(mutableListOf())
-        recyclerView.adapter = adapter
+
+        setSupportActionBar(binding.toolbar)
+        binding.recyclerview.adapter = adapter
         getRepos()
     }
 
@@ -45,11 +37,11 @@ class MainActivity : AppCompatActivity() {
     private fun getRepos() {
 
         disposables.add(viewModel.getTrends()
-                .doOnNext { progress.visibility = View.VISIBLE }
+                .doOnNext { binding.progress.visibility = View.VISIBLE }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    progress.visibility = View.GONE
+                    binding.progress.visibility = View.GONE
                     adapter.swapItems(it) })
     }
 }
